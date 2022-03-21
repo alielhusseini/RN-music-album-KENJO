7afeae3e2aec09e1d731325c1619e186397d680f
Ali El Husseini <alielhusseini@Alis-MacBook-Air.local>
2022-03-21 22:59:57 +0200

    initial commit / chose my colors for the project / thought about the design of the project and what technologies that might be used

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { COLORS } from './assets/colors';
+import { HomeHeader } from './components/shared';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.body}>
+      <StatusBar animated={true} backgroundColor={COLORS.white} barStyle="dark-content" />
+      <HomeHeader />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS.white,
   },
 });
